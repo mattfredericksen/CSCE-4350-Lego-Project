648e01f0d2253429202786d26cc1cd3c8b66ddf2
mattfredericksen <mfred98@gmail.com>
2020-07-14 03:18:54 -0500

    switched to NotImplementedItem

--- menus/main_menu.py
+++ menus/main_menu.py
@@ -2,16 +2,13 @@ from consolemenu import ConsoleMenu
 from consolemenu.items import FunctionItem, SubmenuItem
 
 from .customer_account_menu import account_menu
+from .not_implemented_item import NotImplementedItem
 
 # \/\/\/\/ MAIN MENU \/\/\/\/
 
 main_menu = ConsoleMenu('Welcome to The Lego Store', exit_option_text='Log Out')
 
-browse_item = FunctionItem('Browse Bricks & Sets', input, ['not yet implemented'])
-order_item = FunctionItem('Place an Order', input, ['not yet implemented'])
-
-account_item = SubmenuItem('Account Information', account_menu, menu=main_menu)
-
-main_menu.append_item(browse_item)
-main_menu.append_item(order_item)
-main_menu.append_item(account_item)
+for item in (NotImplementedItem('Browse Bricks & Sets'),
+             NotImplementedItem('Place an Order'),
+             SubmenuItem('Account Information', account_menu, menu=main_menu)):
+    main_menu.append_item(item)
