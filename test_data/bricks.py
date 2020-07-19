2b3a00ba6b557ab4d7e89bb374fa26a0d1a04402
mattfredericksen <mfred98@gmail.com>
2020-07-19 15:31:21 -0500

    Changed random() to uniform()

--- test_data/bricks.py
+++ test_data/bricks.py
@@ -1,4 +1,4 @@
-from random import random, randint, choice
+from random import uniform, randint, choice
 
 
 def generate_bricks():
@@ -7,6 +7,6 @@ def generate_bricks():
 
     return {randint(1, 10000):
             {'description': f'{randint(1,20)}x{randint(1,20)} {choice(colors)}',
-             'price': round(random()*5, 2),
+             'price': round(uniform(.1, 2), 2),
              'inventory': randint(0, 5)}
             for _ in range(1, 100)}
