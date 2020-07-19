4c4060b205e8267946b35c63a89eedb82a2138df
mattfredericksen <mfred98@gmail.com>
2020-07-18 19:54:37 -0500

    Changed list-comprehension of dicts to dict-comprehension

--- test_data/bricks.py
+++ test_data/bricks.py
@@ -5,8 +5,8 @@ def generate_bricks():
     colors = ('red', 'orange', 'yellow', 'green',
               'blue', 'indigo', 'violet', 'white', 'black')
 
-    return [{'id': i,
-             'description': f'{randint(1,20)}x{randint(1,20)} {choice(colors)}',
+    return {randint(1, 10000):
+            {'description': f'{randint(1,20)}x{randint(1,20)} {choice(colors)}',
              'price': round(random()*5, 2),
              'inventory': randint(0, 5)}
-            for i in range(100)]
+            for _ in range(1, 100)}
