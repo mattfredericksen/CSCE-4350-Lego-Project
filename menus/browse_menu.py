3a7d098bb53c1b2d19043dd007c15b46b3994a4d
mattfredericksen <mfred98@gmail.com>
2020-07-19 00:31:27 -0500

    Improved prototype - now supports viewing brick details and adding to cart.

--- menus/browse_menu.py
+++ menus/browse_menu.py
@@ -2,15 +2,11 @@ from consolemenu import ConsoleMenu, SelectionMenu
 from consolemenu.items import FunctionItem, SubmenuItem, SelectionItem
 from consolemenu.menu_formatter import MenuFormatBuilder
 from consolemenu.menu_component import Dimension
+from consolemenu.screen import Screen
+from os import get_terminal_size
 
 from .not_implemented_item import NotImplementedItem
-
 from test_data.bricks import generate_bricks
-
-# TODO: Think about how we want to implement orders.
-#       Perhaps when an item is selected for more details,
-#       we can show an 'add to cart' option with qty input.
-#       Do we want a 'shopping cart' menu instead of 'Place an Order'?
 
 
 class SelectionMenuFromTuples(SelectionMenu):
@@ -19,7 +15,8 @@ class SelectionMenuFromTuples(SelectionMenu):
     def __init__(self, tuples, title=None, subtitle=None, screen=None, formatter=None,
                  prologue_text=None, epilogue_text=None, show_exit_option=True, exit_option_text='Exit'):
         if not formatter:
-            dimension = Dimension(width=max(80, max(len(item[1]) for item in tuples) + 18))
+            width = min(get_terminal_size().columns, max(80, max(len(item[1]) for item in tuples) + 18))
+            dimension = Dimension(width=width)
             formatter = MenuFormatBuilder(max_dimension=dimension)
 
         super(SelectionMenu, self).__init__(title, subtitle, screen=screen, formatter=formatter,
@@ -27,6 +24,36 @@ class SelectionMenuFromTuples(SelectionMenu):
                                             show_exit_option=show_exit_option, exit_option_text=exit_option_text)
         for index, item in tuples:
             self.append_item(SelectionItem(item, index, self))
+
+
+def brick_details(brick_id, bricks):
+    while True:
+        print('ITEM DETAILS\n')
+        for key, value in bricks[brick_id].items():
+            print(f'{key}: {value}')
+        print('\n')  # two newlines
+
+        if (inventory := bricks[brick_id]['inventory']) > 0:
+            print('Enter a quantity to add this item to your cart.')
+        else:
+            print('This item is out of stock at your preferred store.')
+
+        print('Enter nothing to return to browsing.\n')
+
+        try:
+            choice = int(input('>> '))
+        except ValueError:
+            if choice == '':
+                break
+        else:
+            if 1 <= choice <= inventory:
+                # TODO: run SQL to add stuff to cart
+                Screen.clear()
+                input('Added item(s) to cart. Press [enter] to return to browsing.')
+                break
+            elif choice == 0:
+                break
+        Screen.clear()
 
 
 def browse_bricks():
@@ -38,9 +65,12 @@ def browse_bricks():
                                       prologue_text=description,
                                       epilogue_text=description,
                                       exit_option_text='Return')
-    browser.show()
-    input(f'You selected: {bricks[browser.selected_item.index]}\nPress [enter] to exit.')
-    breakpoint()
+    while True:
+        browser.show()
+        if browser.selected_item.text == 'Return':
+            break
+        else:
+            brick_details(browser.selected_item.index, bricks)
 
 
 browse_menu = ConsoleMenu('Browse LEGO Products',
