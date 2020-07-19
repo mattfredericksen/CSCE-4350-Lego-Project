ad50d935f5800e367d3db2c21efad52cd9d1f606
mattfredericksen <mfred98@gmail.com>
2020-07-18 19:55:20 -0500

    Improved prototype

--- menus/browse_menu.py
+++ menus/browse_menu.py
@@ -14,8 +14,14 @@ from test_data.bricks import generate_bricks
 
 
 class SelectionMenuFromTuples(SelectionMenu):
+    """Used for manually providing indexes to list items"""
+    # partially copied from SelectionMenu's __init__()
     def __init__(self, tuples, title=None, subtitle=None, screen=None, formatter=None,
                  prologue_text=None, epilogue_text=None, show_exit_option=True, exit_option_text='Exit'):
+        if not formatter:
+            dimension = Dimension(width=max(80, max(len(item[1]) for item in tuples) + 18))
+            formatter = MenuFormatBuilder(max_dimension=dimension)
+
         super(SelectionMenu, self).__init__(title, subtitle, screen=screen, formatter=formatter,
                                             prologue_text=prologue_text, epilogue_text=epilogue_text,
                                             show_exit_option=show_exit_option, exit_option_text=exit_option_text)
@@ -26,15 +32,15 @@ class SelectionMenuFromTuples(SelectionMenu):
 def browse_bricks():
     description = 'Select an item to view more details or '  \
                   'to add it to your cart'
-    bricks = [(brick['id'], brick['description']) for brick in generate_bricks()]
-    dimension = Dimension(width=max(50, max(len(brick[1]) for brick in bricks) + 18))
-    formatter = MenuFormatBuilder(max_dimension=dimension)
-    browser = SelectionMenuFromTuples(bricks, title='Browse Bricks',
-                                      formatter=formatter,
+    bricks = generate_bricks()
+    bricks_partial = [(key, value['description']) for key, value in bricks.items()]
+    browser = SelectionMenuFromTuples(bricks_partial, title='Browse Bricks',
                                       prologue_text=description,
-                                      epilogue_text=description)
+                                      epilogue_text=description,
+                                      exit_option_text='Return')
     browser.show()
-    input(f'You selected: {browser.selected_item.text}\nPress [enter] to exit.')
+    input(f'You selected: {bricks[browser.selected_item.index]}\nPress [enter] to exit.')
+    breakpoint()
 
 
 browse_menu = ConsoleMenu('Browse LEGO Products',
