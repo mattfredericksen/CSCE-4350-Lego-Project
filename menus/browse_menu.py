6fb481480f2823593394956b1a756446ecb2c726
mattfredericksen <mfred98@gmail.com>
2020-07-19 11:20:41 -0500

    Moved SelectionMenuFromTuples to its own module.

--- menus/browse_menu.py
+++ menus/browse_menu.py
@@ -1,36 +1,17 @@
-from consolemenu import ConsoleMenu, SelectionMenu
-from consolemenu.items import FunctionItem, SubmenuItem, SelectionItem
-from consolemenu.menu_formatter import MenuFormatBuilder
-from consolemenu.menu_component import Dimension
+from consolemenu import ConsoleMenu
+from consolemenu.items import FunctionItem
 from consolemenu.screen import Screen
-from os import get_terminal_size
 
+from .selection_menu import SelectionMenuFromTuples
 from .not_implemented_item import NotImplementedItem
 from test_data.bricks import generate_bricks
-
-
-class SelectionMenuFromTuples(SelectionMenu):
-    """Used for manually providing indexes to list items"""
-    # partially copied from SelectionMenu's __init__()
-    def __init__(self, tuples, title=None, subtitle=None, screen=None, formatter=None,
-                 prologue_text=None, epilogue_text=None, show_exit_option=True, exit_option_text='Exit'):
-        if not formatter:
-            width = min(get_terminal_size().columns, max(80, max(len(item[1]) for item in tuples) + 18))
-            dimension = Dimension(width=width)
-            formatter = MenuFormatBuilder(max_dimension=dimension)
-
-        super(SelectionMenu, self).__init__(title, subtitle, screen=screen, formatter=formatter,
-                                            prologue_text=prologue_text, epilogue_text=epilogue_text,
-                                            show_exit_option=show_exit_option, exit_option_text=exit_option_text)
-        for index, item in tuples:
-            self.append_item(SelectionItem(item, index, self))
 
 
 def brick_details(brick_id, bricks):
     while True:
         print('ITEM DETAILS\n')
         for key, value in bricks[brick_id].items():
-            print(f'{key}: {value}')
+            print(f'{key.title()}: {value}')
         print('\n')  # two newlines
 
         if (inventory := bricks[brick_id]['inventory']) > 0:
@@ -49,7 +30,7 @@ def brick_details(brick_id, bricks):
             if 1 <= choice <= inventory:
                 # TODO: run SQL to add stuff to cart
                 Screen.clear()
-                input('Added item(s) to cart. Press [enter] to return to browsing.')
+                input('Added item(s) to cart (not really). Press [enter] to return to browsing.')
                 break
             elif choice == 0:
                 break
---
+++ menus/selection_menu.py
@@ -0,0 +1,22 @@
+from consolemenu import SelectionMenu
+from consolemenu.items import SelectionItem
+from consolemenu.menu_formatter import MenuFormatBuilder
+from consolemenu.menu_component import Dimension
+from os import get_terminal_size
+
+
+class SelectionMenuFromTuples(SelectionMenu):
+    """Used for manually providing indexes to list items"""
+    # partially copied from SelectionMenu's __init__()
+    def __init__(self, tuples, title=None, subtitle=None, screen=None, formatter=None,
+                 prologue_text=None, epilogue_text=None, show_exit_option=True, exit_option_text='Exit'):
+        if not formatter:
+            width = min(get_terminal_size().columns, max(80, max(len(item[1]) for item in tuples) + 18))
+            dimension = Dimension(width=width)
+            formatter = MenuFormatBuilder(max_dimension=dimension)
+
+        super(SelectionMenu, self).__init__(title, subtitle, screen=screen, formatter=formatter,
+                                            prologue_text=prologue_text, epilogue_text=epilogue_text,
+                                            show_exit_option=show_exit_option, exit_option_text=exit_option_text)
+        for index, item in tuples:
+            self.append_item(SelectionItem(item, index, self))
