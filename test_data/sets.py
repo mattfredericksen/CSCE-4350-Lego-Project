36d44fab4b64367a3d7293181780dddadb513176
mattfredericksen <mfred98@gmail.com>
2020-07-19 13:16:20 -0500

    Improved variability and added inventory to set test data.

--- test_data/sets.py
+++ test_data/sets.py
@@ -1,5 +1,6 @@
 from random import randint, choice
 from .bricks import generate_bricks
+from pprint import pprint
 
 
 def generate_sets():
@@ -7,18 +8,27 @@ def generate_sets():
     brick_ids = list(bricks.keys())
 
     sets = {}
-    for _ in range(1, 20):
+    for _ in range(20):
         key = randint(1, 10000)
         name = f'Set #{key}'
-        description = 'Super creative description goes here'
-        items = {choice(brick_ids): randint(5, 15) for _ in range(5, 15)}
-        price = round(sum(bricks[item]['price'] * qty for item, qty in items.items()), 2)
+        description = f'Super creative description of set #{key} goes here.'
+        inventory = randint(0, 5)
+        set_items = {choice(brick_ids): randint(5, 15) for _ in range(randint(5, 15))}
+        price = round(sum(bricks[item]['price'] * qty for item, qty in set_items.items()), 2)
         sets.update({
             key: {
                 'name': name,
                 'description': description,
-                'set_items': items,
+                'inventory': inventory,
+                'set_items': set_items,
                 'price': price
             }
         })
     return {'sets': sets, 'bricks': bricks}
+
+
+def print_set_data():
+    data = generate_sets()
+    pprint(data['sets'])
+    print()
+    pprint(data['bricks'])
