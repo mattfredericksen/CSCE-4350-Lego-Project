36759800500a3b421f753f87535c6ef043819a47
mattfredericksen <mfred98@gmail.com>
2020-07-20 13:06:57 -0500

    Adjusted menu options. Plugged in Sale menu.

--- menus/employee_main_menu.py
+++ menus/employee_main_menu.py
@@ -2,12 +2,15 @@ from consolemenu import ConsoleMenu
 from consolemenu.items import FunctionItem, SubmenuItem
 
 from .not_implemented_item import NotImplementedItem
+from menufunctions.sale import sale
+
 
 # \/\/\/\/ MAIN MENU \/\/\/\/
 
 employee_main_menu = ConsoleMenu('Welcome, working-class scum', exit_option_text='Log Out')
 
-for item in (NotImplementedItem('Sales'),
+for item in (FunctionItem('Start a Sale', sale),
+             NotImplementedItem('Start a Return'),
              NotImplementedItem('Order Management'),
              NotImplementedItem('Delivery Management')):
     employee_main_menu.append_item(item)
