205e52f198b262c60ed6aeb548de0a94a31f7b3f
mattfredericksen <mfred98@gmail.com>
2020-07-21 16:51:09 -0500

    Relocated store management functions

---
+++ menufunctions/store.py
@@ -0,0 +1,43 @@
+from test_data.static import stores, employees
+
+
+def view(context: dict):
+    print('VIEWING STORES\n')
+
+    for store_id, store in stores.items():
+        # currently assumes that all stores have managers
+        # manager = employees[store["manager"]]["name"] if store["manager"] else None
+        count = len([e for e in employees.values() if e['store'] == store_id])
+        print(f'Store ID: {store_id}:\n'
+              f'\tAddress: {store["address"]}\n'
+              f'\tManager: {employees[store["manager"]]["name"]}\n'
+              f'\tEmployee Count: {count}\n')
+
+    input('Press [enter] to return.')
+
+
+def create(context: dict):
+    print('CREATING NEW STORE\n'
+          '[entering a blank field cancels this process]\n')
+
+    address = input('Store address: ')
+    if not address:
+        return
+
+    manager_id = None
+    while True:
+        manager_id = input('Manager\'s employee ID: ')
+        try:
+            manager_id = int(manager_id)
+        except ValueError:
+            if manager_id == '':
+                return
+        else:
+            if manager_id in employees:
+                break
+        print(f'\nInvalid employee ID: "{manager_id}"')
+
+    # TODO: run SQL to insert new store
+
+    input('Store created.\n'
+          'Press [enter] to return.')
