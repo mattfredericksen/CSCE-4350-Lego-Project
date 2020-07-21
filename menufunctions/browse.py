825f7e52e918900f55ed5cc1f0d1201d29e4033d
mattfredericksen <mfred98@gmail.com>
2020-07-20 22:16:44 -0500

    Combined browse_sets and browse_bricks into a single browse function.

--- menufunctions/browse.py
+++ menufunctions/browse.py
@@ -3,36 +3,29 @@
 from menus.selection_menu import SelectionMenuFromTuples
 from .details import brick_details, set_details
 
+from typing import Literal
+
 from test_data.static import bricks, sets
 
 
-def browse_bricks():
+def browse(mode: Literal['Set', 'Brick']):
     description = 'Select an item to view more details or '  \
                   'to add it to your cart'
-    bricks_partial = [(key, value['description']) for key, value in bricks.items()]
-    browser = SelectionMenuFromTuples(bricks_partial, title='Browse Bricks',
+    if mode is 'Set':
+        partial = [(key, value['name']) for key, value in sets.items()]
+    else:
+        partial = [(key, value['description']) for key, value in bricks.items()]
+
+    browser = SelectionMenuFromTuples(partial, title=f'Browse {mode}s',
                                       prologue_text=description,
                                       epilogue_text=description,
                                       exit_option_text='Return')
+
     while True:
         browser.show()
         if browser.selected_item is browser.exit_item:
             break
+        elif mode is 'Set':
+            set_details(browser.selected_item.index)
         else:
             brick_details(browser.selected_item.index)
-
-
-def browse_sets():
-    description = 'Select a set to view more details or '  \
-                  'to add it to your cart'
-    sets_partial = [(key, value['name']) for key, value in sets.items()]
-    browser = SelectionMenuFromTuples(sets_partial, title='Browse Sets',
-                                      prologue_text=description,
-                                      epilogue_text=description,
-                                      exit_option_text='Return')
-    while True:
-        browser.show()
-        if browser.selected_item is browser.exit_item:
-            break
-        else:
-            set_details(browser.selected_item.index)
--- menus/browse_menu.py
+++ menus/browse_menu.py
@@ -1,13 +1,13 @@
 from consolemenu import ConsoleMenu
 from consolemenu.items import FunctionItem
 
-from menufunctions.browse import browse_bricks, browse_sets
+from menufunctions.browse import browse
 from menufunctions.search import search
 
 browse_menu = ConsoleMenu('Browse & Search LEGO Products',
                           exit_option_text='Return to Main Menu')
 
-for item in (FunctionItem('Browse Bricks', browse_bricks),
-             FunctionItem('Browse Sets', browse_sets),
+for item in (FunctionItem('Browse Bricks', browse, ('Brick',)),
+             FunctionItem('Browse Sets', browse, ('Set',)),
              FunctionItem('Search All', search)):
     browse_menu.append_item(item)
