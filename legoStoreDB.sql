b2fa36639e708f93c748cb8ba07b78a9d88db0f4
mattfredericksen <mfred98@gmail.com>
2020-07-22 22:36:37 -0500

    Adjusted table and attribute names. Corrected data types. Added constraints.

--- legoStoreDB.sql
+++ legoStoreDB.sql
@@ -1,184 +1,216 @@
 CREATE database LegoCo
 
 
-CREATE TABLE Store (
-	storeID int NOT NULL,
-	address varchar(255) NOT NULL,
-	managerID int NOT NULL,
-	PRIMARY KEY (storeID)
-);
-
-CREATE TABLE Customer (
-	customerID int NOT NULL,
-	email varchar(255)NOT NULL UNIQUE,
-	username varchar(255) NOT NULL UNIQUE,
-	password varchar(255) NOT NULL,
-	name varchar(255) NOT NULL,
-	address varchar(255) NOT NULL,
-	storePref int, 
-	PRIMARY KEY (customerID),
-	FOREIGN KEY (storePref) REFERENCES Store (storeID)
-);
-
-CREATE TABLE Payments (
-	cardNumber int NOT NULL,	
-	customerID int, 
-	cardType varchar(255) NOT NULL,
-	expDate varchar(255) NOT NULL,
-	billingAddress varchar(255) NOT NULL,
-	active char NOT NULL,
-	PRIMARY KEY (cardNumber),
-	FOREIGN KEY (customerID) REFERENCES Customer (customerID)
-);
-
-CREATE TABLE onlineOrders (
-	orderID int NOT NULL, 
-	customerID int,
-	cardNumber int, 
-	orderTimestamp varchar(255) NOT NULL,
-	deliveryTimestamp varchar(255) NOT NULL,
-	status varchar(255) NOT NULL,
-	totalPrice float NOT NULL,				/*derived*/
-	PRIMARY KEY (orderID),
-	FOREIGN KEY (customerID) REFERENCES Customer (customerID),
-	FOREIGN KEY (cardNumber) REFERENCES Payments (cardNumber)
+CREATE TABLE Stores (
+	store_id INT NOT NULL AUTO_INCREMENT,
+	address VARCHAR(256) NOT NULL,
+	manager_id INT,
+    PRIMARY KEY (store_id)
 );
 
 CREATE TABLE Employees (
-	employeeID int NOT NULL, 
-	name varchar(255) NOT NULL,
-	storeID int, 
-	PRIMARY KEY (employeeID),
-	FOREIGN KEY (storeID) REFERENCES Store (storeID)
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    store_id INT NOT NULL,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (store_id) REFERENCES Stores (store_id)
 );
 
-CREATE TABLE inStoreSales (
-	saleID int NOT NULL, 
-	storeID int,
-	employeeID int,
-	saleTimestamp varchar (255) NOT NULL,
-	totalPrice float NOT NULL,				/*derived*/
-	PRIMARY KEY (saleID),
-	FOREIGN KEY (storeID) REFERENCES Store (storeID),
-	FOREIGN KEY (employeeID) REFERENCES Employees (employeeID)
-);
+ALTER TABLE Stores
+ADD FOREIGN KEY (manager_id) REFERENCES Employees (employee_id);
 
-CREATE TABLE legoSets (
-	setID int NOT NULL,
-	description varchar (1023),
-	price float NOT NULL,					/*derived*/
-	PRIMARY KEY (setID)
+CREATE TABLE Sets (
+	set_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+	description VARCHAR(512) NOT NULL,
+    active BOOLEAN,
+	PRIMARY KEY (set_id)
 );
 
 CREATE TABLE Bricks (
-	partID int NOT NULL, 
-	description varchar (1023),
-	price float NOT NULL,
-	PRIMARY KEY (partID)
+	brick_id INT NOT NULL AUTO_INCREMENT,
+	description VARCHAR(256) NOT NULL,
+    price FLOAT NOT NULL,
+    active BOOLEAN,
+	PRIMARY KEY (brick_id),
+    CHECK (price > 0.0)
 );
 
-CREATE TABLE StoreSets (
-	storeID int,
-	setID int,
-	inventory int NOT NULL,
-	availableInventory int NULL,
-	FOREIGN KEY (storeID) REFERENCES Store (storeID),
-	FOREIGN KEY (setID) REFERENCES legoSets (setID)
+CREATE TABLE Set_Bricks (
+	set_id INT NOT NULL,
+	brick_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (set_id) REFERENCES Sets (set_id),
+	FOREIGN KEY (brick_id) REFERENCES Bricks (brick_id),
+    PRIMARY KEY (set_id, brick_id),
+    CHECK (quantity > 0)
 );
 
-CREATE TABLE StoreBricks (
-	storeID int,
-	partID int,
-	inventory int NOT NULL,
-	availableInventory int NULL,
-	FOREIGN KEY (storeID) REFERENCES Store (storeID),
-	FOREIGN KEY (partID) REFERENCES Bricks (partID)
+CREATE TABLE Stores_Bricks (
+	store_id INT NOT NULL,
+	brick_id INT NOT NULL,
+	inventory INT NOT NULL,
+	FOREIGN KEY (store_id) REFERENCES Stores (store_id),
+	FOREIGN KEY (brick_id) REFERENCES Bricks (brick_id)
+    /* no checks here because we don't want to
+       cause issues if inventory goes negative */
 );
 
-CREATE TABLE SaleSets (
-	saleID int,
-	setID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (saleID) REFERENCES inStoreSales (saleID),
-	FOREIGN KEY (setID) REFERENCES legoSets (setID)
+CREATE TABLE Stores_Sets (
+	store_id INT NOT NULL,
+	set_id INT NOT NULL,
+	inventory INT NOT NULL,
+	FOREIGN KEY (store_id) REFERENCES Stores (store_id),
+	FOREIGN KEY (set_id) REFERENCES Sets (set_id)
+    /* no checks here because we don't want to
+       cause issues if inventory goes negative */
 );
 
-CREATE TABLE OrderSets (
-	orderID int,
-	setID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (orderID) REFERENCES onlineOrders (orderID),
-	FOREIGN KEY (setID) REFERENCES legoSets (setID)
+CREATE TABLE Customers (
+	customer_id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(128) NOT NULL UNIQUE,
+	username VARCHAR(128) NOT NULL UNIQUE,
+	password VARCHAR(128) NOT NULL,
+	name VARCHAR(128) NOT NULL,
+	address VARCHAR(256) NOT NULL,
+	store_preference INT NOT NULL,
+    PRIMARY KEY (customer_id),
+	FOREIGN KEY (store_preference) REFERENCES Stores (store_id)
 );
 
-CREATE TABLE SaleBricks (
-	saleID int,
-	partID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (saleID) REFERENCES inStoreSales (saleID),
-	FOREIGN KEY (partID) REFERENCES Bricks (partID)
+CREATE TABLE Payments (
+	customer_id INT NOT NULL,
+	card_number CHAR(16) NOT NULL,
+	exp_date DATE NOT NULL,
+	billing_address VARCHAR(256) NOT NULL,
+	active BOOLEAN NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+	PRIMARY KEY (customer_id, card_number)
 );
 
-CREATE TABLE OrderBricks (
-	orderID int,
-	partID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (orderID) REFERENCES onlineOrders (orderID),
-	FOREIGN KEY (partID) REFERENCES Bricks (partID)
+CREATE TABLE Customer_Orders (
+	order_id INT NOT NULL AUTO_INCREMENT,
+	customer_id INT NOT NULL,
+	card_number CHAR(16) NOT NULL,
+    store_id INT NOT NULL,
+	order_timestamp DATETIME,
+	delivery_timestamp DATETIME,
+	status VARCHAR(16) NOT NULL,
+	total_price FLOAT NOT NULL,
+	PRIMARY KEY (order_id),
+	FOREIGN KEY (customer_id, card_number)
+        REFERENCES Payments (customer_id, card_number),
+    CHECK (delivery_timestamp IS NULL OR
+           delivery_timestamp > order_timestamp),
+    CHECK (status IN ('Processing', 'Shipping',
+                      'Delivered', 'Cancelled',
+                      'Returned', 'Cart')),
+    CHECK (total_price > 0.0)
 );
 
-CREATE TABLE Supplier (
-	supplierID int NOT NULL,
-	name varchar(255) NOT NULL,
-	PRIMARY KEY (supplierID)
+CREATE TABLE Customer_Order_Returns (
+	order_id INT NOT NULL,
+	reason VARCHAR(256),
+	timestamp DATETIME NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES Customer_Orders (order_id),
+    PRIMARY KEY (order_id)
 );
 
-CREATE TABLE StoreOrder (
-	storeOrderID int NOT NULL,
-	storeID int,
-	supplierID int,
-	orderTimestamp varchar(255) NOT NULL,
-	deliveryTimestamp varchar(255) NOT NULL,
-	status varchar(255) NOT NULL,
-	PRIMARY KEY (storeOrderID),
-	FOREIGN KEY (storeID) REFERENCES Store (storeID),
-	FOREIGN KEY (supplierID) REFERENCES Supplier (supplierID)
+CREATE TABLE Customer_Orders_Bricks (
+	order_id INT NOT NULL,
+	brick_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES Customer_Orders (order_id),
+	FOREIGN KEY (brick_id) REFERENCES Bricks (brick_id),
+    PRIMARY KEY (order_id, brick_id),
+    CHECK (quantity > 0)
 );
 
-CREATE TABLE StoreOrderBricks (
-	storeOrderID int,
-	partID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (storeOrderID) REFERENCES StoreOrder (storeOrderID),
-	FOREIGN KEY (partID) REFERENCES Bricks (partID)
+CREATE TABLE Customer_Orders_Sets (
+	order_id INT NOT NULL,
+	set_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES Customer_Orders (order_id),
+	FOREIGN KEY (set_id) REFERENCES Sets (set_id),
+    PRIMARY KEY (order_id, set_id),
+    CHECK (quantity > 0)
 );
 
-CREATE TABLE StoreOrderSets (
-	storeOrderID int,
-	setID int,
-	quantity int NOT NULL,
-	FOREIGN KEY (storeOrderID) REFERENCES StoreOrder (storeOrderID),
-	FOREIGN KEY (setID) REFERENCES legoSets (setID)
+CREATE TABLE Store_Sales (
+	sale_id INT NOT NULL AUTO_INCREMENT,
+	store_id INT NOT NULL,
+	employee_id INT NOT NULL,
+	sale_timestamp DATETIME NOT NULL,
+	total_price FLOAT NOT NULL,
+    credit_card CHAR(16),
+	PRIMARY KEY (sale_id),
+	FOREIGN KEY (store_id) REFERENCES Stores (store_id),
+	FOREIGN KEY (employee_id) REFERENCES Employees (employee_id),
+    CHECK (total_price > 0.0)
 );
 
-CREATE TABLE CustomerOrderReturn (
-	orderID int,
-	reason varchar(255),
-	timestamp varchar(255) NOT NULL,
-	FOREIGN KEY (orderID) REFERENCES onlineOrders (orderID)	
+CREATE TABLE Store_Sales_Returns (
+	sale_id INT NOT NULL,
+	reason VARCHAR(256),
+	return_timestamp DATETIME NOT NULL,
+	FOREIGN KEY (sale_id) REFERENCES Store_Sales (sale_id),
+    PRIMARY KEY (sale_id)
 );
 
-CREATE TABLE StoreSaleReturn (
-	orderID int,
-	reason varchar(255),
-	timestamp varchar(255) NOT NULL,
-	FOREIGN KEY (orderID) REFERENCES inStoreSales (saleID)	
+CREATE TABLE Store_Sales_Bricks (
+	sale_id INT NOT NULL,
+	brick_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (sale_id) REFERENCES Store_Sales (sale_id),
+	FOREIGN KEY (brick_id) REFERENCES Bricks (brick_id),
+    PRIMARY KEY (sale_id, brick_id),
+    CHECK (quantity > 0)
 );
 
-CREATE TABLE SetBricks (
-	setID int,
-	brickID int,
-	quantity int,
-	FOREIGN KEY (setID) REFERENCES legoSets (setID),
-	FOREIGN KEY (brickID) REFERENCES Bricks (partID)
+CREATE TABLE Store_Sales_Sets (
+	sale_id INT NOT NULL,
+	set_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (sale_id) REFERENCES Store_Sales (sale_id),
+	FOREIGN KEY (set_id) REFERENCES Sets (set_id),
+    PRIMARY KEY (sale_id, set_id),
+    CHECK (quantity > 0)
+);
+
+CREATE TABLE Suppliers (
+	supplier_id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(128) NOT NULL,
+	PRIMARY KEY (supplier_id)
+);
+
+CREATE TABLE Store_Orders (
+	order_id INT NOT NULL AUTO_INCREMENT,
+	store_id INT NOT NULL,
+	supplier_id INT NOT NULL,
+	order_timestamp DATETIME,
+	delivery_timestamp DATETIME,
+	status VARCHAR(16) NOT NULL,
+	PRIMARY KEY (order_id),
+	FOREIGN KEY (store_id) REFERENCES Stores (store_id),
+	FOREIGN KEY (supplier_id) REFERENCES Suppliers (supplier_id),
+    CHECK (status IN ('Ordered', 'Delivered', 'Cancelled'))
+);
+
+CREATE TABLE Store_Orders_Bricks (
+	order_id INT NOT NULL,
+	brick_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES Store_Orders (order_id),
+	FOREIGN KEY (brick_id) REFERENCES Bricks (brick_id),
+    PRIMARY KEY (order_id, brick_id),
+    CHECK (quantity > 0)
+);
+
+CREATE TABLE Store_Orders_Sets (
+	order_id INT NOT NULL,
+	set_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES Store_Orders (order_id),
+	FOREIGN KEY (set_id) REFERENCES Sets (set_id),
+    CHECK (quantity > 0)
 );
