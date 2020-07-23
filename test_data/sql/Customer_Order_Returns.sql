b24bdb1ad41ae6d7e8dbff68063817238f9443c3
RawToaster <66383650+RawToaster@users.noreply.github.com>
2020-07-23 15:12:04 -0500

    Fixed typo

--- test_data/sql/Customer_Order_Returns.sql
+++ test_data/sql/Customer_Order_Returns.sql
@@ -1,5 +1,5 @@
-insert into Customer_Order_Returns (order_id, Reason, return-timestamp) values (13, 'Order not fully filled', '2122/75/7 86:46:6');
-insert into Customer_Order_Returns (order_id, Reason, return-timestamp) values (14, 'Unwanted items', '2122/5/91 81:4:3');
-insert into Customer_Order_Returns (order_id, Reason, return-timestamp) values (11, 'Order not fully filled', '2222/0/47 5:7:3');
-insert into Customer_Order_Returns (order_id, Reason, return-timestamp) values (5, 'broken', '2021/0/6 38:98:34');
-insert into Customer_Order_Returns (order_id, Reason, return-timestamp) values (10, 'broken', '2022/75/08 32:0:34');
+insert into Customer_Order_Returns (order_id, Reason, return_timestamp) values (13, 'Order not fully filled', '2122/75/7 86:46:6');
+insert into Customer_Order_Returns (order_id, Reason, return_timestamp) values (14, 'Unwanted items', '2122/5/91 81:4:3');
+insert into Customer_Order_Returns (order_id, Reason, return_timestamp) values (11, 'Order not fully filled', '2222/0/47 5:7:3');
+insert into Customer_Order_Returns (order_id, Reason, return_timestamp) values (5, 'broken', '2021/0/6 38:98:34');
+insert into Customer_Order_Returns (order_id, Reason, return_timestamp) values (10, 'broken', '2022/75/08 32:0:34');
