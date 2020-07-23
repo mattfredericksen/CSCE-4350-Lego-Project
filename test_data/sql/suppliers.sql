c2ad6f1d217e300edfc32c54aacf2e424e69a2a6
RawToaster <66383650+RawToaster@users.noreply.github.com>
2020-07-23 15:41:21 -0500

    Create suppliers.sql

---
+++ test_data/sql/suppliers.sql
@@ -0,0 +1,5 @@
+insert into Suppliers (name) values ('Gigabox');
+insert into Suppliers (name) values ('Centidel');
+insert into Suppliers (name) values ('Lazzy');
+insert into Suppliers (name) values ('Jaloo');
+insert into Suppliers (name) values ('Roodel');
